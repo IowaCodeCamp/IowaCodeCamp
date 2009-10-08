d3a528a9d8000345e613c66da3d2accd9bbc0e96
Chris Sutton <bdsutton@gmail.com>
2009-10-08 00:39:58 +0000

    remove unused mvc files

--- src/Website/Services/IAuthentication.cs
+++
@@ -1,8 +0,0 @@
-namespace IowaCodeCamp.Website.Services
-{
-    public interface IAuthentication
-    {
-        void SignIn(string userName, bool createPersistentCookie);
-        void SignOut();
-    }
-}
