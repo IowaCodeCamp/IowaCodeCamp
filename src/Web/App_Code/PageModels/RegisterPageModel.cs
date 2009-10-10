8aac399b7b9d3a1fc14d78129123dbaf5f1063f8
Chris Sutton <bdsutton@gmail.com>
2009-10-10 20:43:14 +0000

    just some better formatting to make annotations easier to read

--- src/Web/App_Code/PageModels/RegisterPageModel.cs
+++ src/Web/App_Code/PageModels/RegisterPageModel.cs
@@ -11,16 +11,23 @@ public class RegisterPageModel
 {
     [Required,StringLength(50)]
     public string FirstName { get; set; }
+
     [Required,StringLength(50)]
     public string LastName { get; set; }
+
     [Required,StringLength(50),DataType(DataType.EmailAddress)]
     public string Email { get; set; }
+
     [Required, StringLength(50)]
     public string City { get; set; }
+
     [Required, StringLength(50)]
     public string Region { get; set; }
+
     [Required, StringLength(50)]
     public string Organization { get; set; }
+
     public string Website { get; set; }
+
     public string Comments { get; set; }
 }
