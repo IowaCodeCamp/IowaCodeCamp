a838060643bd1209ea0fb069341462a4494d0a6a
Chris Sutton <bdsutton@gmail.com>
2009-10-10 18:01:13 +0000

    update the view model with data annotations (probably won't leave validation in annotations)

--- src/Web/App_Code/PageModels/RegisterPageModel.cs
+++ src/Web/App_Code/PageModels/RegisterPageModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,17 @@ using System.Web;
 /// </summary>
 public class RegisterPageModel
 {
+    [Required,StringLength(50)]
     public string FirstName { get; set; }
+    [Required,StringLength(50)]
     public string LastName { get; set; }
+    [Required,StringLength(50),DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    [Required, StringLength(50)]
     public string City { get; set; }
+    [Required, StringLength(50)]
     public string Region { get; set; }
+    [Required, StringLength(50)]
     public string Organization { get; set; }
     public string Website { get; set; }
     public string Comments { get; set; }
