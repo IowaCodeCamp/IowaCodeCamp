9f9c3e7e4a26d85966ad6d2c2140a70bcc7abfdc
Chris Sutton <bdsutton@gmail.com>
2009-10-12 02:48:10 +0000

    move about page to mvc

---
+++ src/Web/App_Code/Controllers/AboutController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+public class AboutController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ src/Web/App_Code/Controllers/ErrorController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+public class ErrorController : Controller
+{
+    public ActionResult NotFound()
+    {
+        return View();
+    }
+}
