151de8aed41b0b8f56b65d1441d757cb5ee7340f
Chris Sutton <bdsutton@gmail.com>
2009-10-13 04:59:27 +0000

    move images around

--- src/Web/App_Code/BasePage.cs
+++ src/Web/App_Code/BasePage.cs
@@ -3,6 +3,6 @@ public class BasePage : System.Web.Mvc.ViewPage
 {
     public virtual string SpeakerImageUrl(string fileName)
     {
-        return ResolveUrl("~/content/images/speakers/fall2009/{0}".FormatWith(fileName));
+        return ResolveUrl("~/public/images/speakers/fall2009/{0}".FormatWith(fileName));
     }
 }
