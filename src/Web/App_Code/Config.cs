3051208542a51955a01505298e3cff454fb32036
Chris Sutton <bdsutton@gmail.com>
2009-10-27 12:02:00 +0000

    updated to 250 max attendees

--- src/Web/App_Code/Config.cs
+++ src/Web/App_Code/Config.cs
@@ -11,7 +11,7 @@ public class Config
 {
     public static int MaxAttendees
     {
-        get { return int.Parse(Get("MaxAttendees", "3")); }
+        get { return int.Parse(Get("MaxAttendees", "250")); }
     }
 
     private static string Get(string key, string @default)
